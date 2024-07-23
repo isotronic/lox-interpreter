c7be440e1bba019f970ca344b73f1dd60330b1da
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-23 07:56:37 +0100

    scanning: other single-character tokens

--- app/main.py
+++ app/main.py
@@ -11,6 +11,18 @@ def scan_file(contents):
             print("LEFT_BRACE { null")
         elif char == "}":
             print("RIGHT_BRACE } null")
+        elif char ==",":
+            print("COMMA , null")
+        elif char == ";":
+            print("SEMICOLON ; null")
+        elif char == "+":
+            print("PLUS + null")
+        elif char == "-":
+            print("MINUS - null")
+        elif char == "*":
+            print("STAR * null")
+        elif char == ".":
+            print("DOT . null")
 
 def main():
     print("Logs from your program will appear here!", file=sys.stderr)
