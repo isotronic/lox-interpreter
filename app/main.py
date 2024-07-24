9149d8f14ae8c29a38ec57949f36d9ddc8f296bf
Joseph Bouqdib <joseph@bouqdib.com>
2024-07-24 07:28:02 +0100

    fix: codecrafters import constants from file not working

--- app/main.py
+++ app/main.py
@@ -1,6 +1,47 @@
 import sys
 import pathlib
-from constants import TokenType, SINGLE_CHAR_TOKENS, RESERVED_WORDS
+# from constants import TokenType, SINGLE_CHAR_TOKENS, RESERVED_WORDS
+from enum import Enum, auto
+
+class TokenType(Enum):
+    LEFT_PAREN = auto()
+    RIGHT_PAREN = auto()
+    LEFT_BRACE = auto()
+    RIGHT_BRACE = auto()
+    COMMA = auto()
+    SEMICOLON = auto()
+    PLUS = auto()
+    MINUS = auto()
+    STAR = auto()
+    DOT = auto()
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    BANG = auto()
+    BANG_EQUAL = auto()
+    LESS = auto()
+    LESS_EQUAL = auto()
+    GREATER = auto()
+    GREATER_EQUAL = auto()
+    SLASH = auto()
+    STRING = auto()
+    NUMBER = auto()
+    IDENTIFIER = auto()
+    EOF = auto()
+
+RESERVED_WORDS = ["and", "class", "else", "false", "for", "fun", "if", "nil", "or", "print", "return", "super", "this", "true", "var", "while"]
+
+SINGLE_CHAR_TOKENS = {
+    '(': TokenType.LEFT_PAREN,
+    ')': TokenType.RIGHT_PAREN,
+    '{': TokenType.LEFT_BRACE,
+    '}': TokenType.RIGHT_BRACE,
+    ',': TokenType.COMMA,
+    ';': TokenType.SEMICOLON,
+    '+': TokenType.PLUS,
+    '-': TokenType.MINUS,
+    '*': TokenType.STAR,
+    '.': TokenType.DOT,
+}
 
 def scan_file(contents):
     """
